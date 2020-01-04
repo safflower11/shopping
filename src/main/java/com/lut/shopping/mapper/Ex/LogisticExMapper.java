104a6f0dbc29cd60635a6509ce50d56c477b978f
liyw <2812403465@qq.com>
2020-01-04 16:29:46 +0800

    新增

--- src/main/java/com/lut/shopping/mapper/Ex/LogisticExMapper.java
+++ src/main/java/com/lut/shopping/mapper/Ex/LogisticExMapper.java
@@ -1,5 +1,12 @@
 package com.lut.shopping.mapper.Ex;
 
+import com.lut.shopping.bean.Ex.LogisticEx;
+
+import java.util.List;
+
 public interface LogisticExMapper {
+    List<LogisticEx> selectAll();
+
+
 
 }
