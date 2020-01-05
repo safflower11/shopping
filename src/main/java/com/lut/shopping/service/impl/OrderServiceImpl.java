f198a4428e3d2cf6e5a8ccdf1e8dc5216ee2e17b
suixr <1150165271@qq.com>
2020-01-05 16:04:16 +0800

    sxrrr

--- src/main/java/com/lut/shopping/service/impl/OrderServiceImpl.java
+++ src/main/java/com/lut/shopping/service/impl/OrderServiceImpl.java
@@ -7,22 +7,23 @@ import com.lut.shopping.mapper.Ex.CoExMapper;
 import com.lut.shopping.mapper.Ex.OrderExMapper;
 import com.lut.shopping.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 import java.util.List;
-
+@Service
 public class OrderServiceImpl implements IOrderService {
     @Autowired
     private OrderExMapper orderExMapper;
-
-    @Override
-    public OrderEx findById(int id) throws RuntimeException {
-        return orderExMapper.findById(id);
-    }
     @Autowired
     private CoExMapper coExMapper;
     @Autowired
     private CoMapper coMapper;
+    @Override
+    public OrderEx findById(int id) throws RuntimeException {
+        return orderExMapper.findById(id);
+    }
+
     @Override
     public List<CoEx> findAll() throws RuntimeException {
         List<CoEx> list= coExMapper.selectAll();
