4d617cf1e8ede9aac4cac4cbd5895045c740def0
lirl <2849569929@qq.com>
2020-01-05 21:57:22 +0800

    xsx

--- src/main/java/com/lut/shopping/bean/Ex/CommodityEx.java
+++ src/main/java/com/lut/shopping/bean/Ex/CommodityEx.java
@@ -4,14 +4,50 @@ import java.io.Serializable;
 import java.util.List;
 
 public class CommodityEx implements Serializable {
+    private Integer id;
+    private String name;
+    private String type;
+    private Double price;
 
-    private String cname;
+    private String shname;
 
-    public String getCname() {
-        return cname;
+    public Integer getId() {
+        return id;
     }
 
-    public void setCname(String cname) {
-        this.cname = cname;
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getShname() {
+        return shname;
+    }
+
+    public void setShname(String shname) {
+        this.shname = shname;
     }
 }
